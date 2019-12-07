f39764eff240f4c020afaf943fd6feee4c29581a
Christian Jauvin <cjauvin@gmail.com>
2019-12-06 19:58:09 -0500

    Day 6 for Python 3.8

--- d06.py
+++ d06.py
@@ -1,67 +1,20 @@
-s = """COM)B
-B)C
-C)D
-D)E
-E)F
-B)G
-G)H
-D)I
-E)J
-J)K
-K)L
-"""
-
-s = """COM)B
-B)C
-C)D
-D)E
-E)F
-B)G
-G)H
-D)I
-E)J
-J)K
-K)L
-K)YOU
-I)SAN
-"""
-
 tree = {}
 
-# for line in s.split("\n"):
 for line in open("d06_input.txt"):
-    if not line.strip():
-        continue
-    a, b = line.strip().split(")")
-    # print(f"{a}, {b}")
-    tree[b] = a
+    inner, outer = line.strip().split(")")
+    tree[outer] = inner
 
 
-def steps_to_com(a):
-    steps = []
-    while a != "COM":
-        steps.append(a)
-        a = tree[a]
+def steps_to_com(o):
+    steps = set()
+    while o != "COM":
+        steps.add(o := tree[o])  # warning this is Py3.8+ only!
     return steps
 
 
-# assert len(steps_to_com("D")) == 3
-# assert len(steps_to_com("L")) == 7
-# assert len(steps_to_com("COM")) == 0
+print("Part 1:", sum(len(steps_to_com(o)) for o in tree.keys()))
 
-# # Part 1
-n = 0
-for k in tree.keys():
-    n += len(steps_to_com(k))
+you = steps_to_com("YOU")
+san = steps_to_com("SAN")
 
-print(n)
-
-# print(steps_to_com("YOU"))
-# print(steps_to_com("SAN"))
-
-# Part 2
-
-you = set(steps_to_com("YOU")[1:])
-san = set(steps_to_com("SAN")[1:])
-
-print(len((san | you) - (san & you)))
+print("Part 2:", len((san | you) - (san & you)))
