f72f32a916f00fb34dbc1a4f274a84f08d66fb02
Christian Jauvin <cjauvin@gmail.com>
2019-12-26 10:00:26 -0500

    Update Day 15

--- d15.py
+++ d15.py
@@ -87,7 +87,7 @@ def execute(source):
 
 def draw():
     extent = [float("inf"), float("-inf"), float("inf"), float("-inf")]
-    for p in walls | visited:
+    for p in g.keys():
         extent = [
             min(extent[0], p[0]),
             max(extent[1], p[0]),
@@ -96,34 +96,16 @@ def draw():
         ]
     # print(extent)
     for y in range(extent[2] - 1, extent[3] + 1):
-        row = []
-        for x in range(extent[0] - 1, extent[1] + 1):
-            # print(x, y)
-            if (x, y) in walls:
-                row.append("#")
-            elif (x, y) in oxygen:
-                row.append("O")
-            elif (x, y) in visited:
-                row.append(".")
-            else:
-                row.append(" ")
+        row = [g[(x, y)] for x in range(extent[0] - 1, extent[1] + 1)]
         print("".join(row))
     print()
 
 
-DIRS = {1: "north", 2: "south", 3: "west", 4: "east"}
-opposite_dir = {1: 2, 2: 1, 3: 4, 4: 3}
-
-
-robot = execute(open("d15_input.txt").read())
-visited = set()
-walls = set()
-oxygen = set()
-
-
 def visit(pos, from_direction=None, steps=1):
-    global robot, visited, walls, oxygen_pos
-    visited.add(pos)
+    global robot, g
+    assert g[pos] != "#"
+    if g[pos] == " ":
+        g[pos] = "."
     for direction in [1, 2, 3, 4]:
         if direction == 1:
             candidate_pos = (pos[0], pos[1] - 1)
@@ -133,17 +115,17 @@ def visit(pos, from_direction=None, steps=1):
             candidate_pos = (pos[0] - 1, pos[1])
         elif direction == 4:
             candidate_pos = (pos[0] + 1, pos[1])
-        if candidate_pos not in walls and candidate_pos not in visited:
+        if g[candidate_pos] == " ":
             next(robot)
             answer = robot.send(direction)
             # print(pos, direction, answer)
             assert answer in [0, 1, 2]
             if answer == 0:
-                walls.add(candidate_pos)
+                g[candidate_pos] = "#"
             elif answer == 1:
                 visit(candidate_pos, direction, steps + 1)
             elif answer == 2:
-                oxygen.add(candidate_pos)
+                g[candidate_pos] = "O"
                 print(f"Part 1: found oxygen in {steps} steps at {candidate_pos}")
                 visit(candidate_pos, direction, steps + 1)
     if from_direction:
@@ -152,13 +134,17 @@ def visit(pos, from_direction=None, steps=1):
         assert answer == 1
 
 
+robot = execute(open("d15_input.txt").read())
+g = defaultdict(lambda: " ")
+opposite_dir = {1: 2, 2: 1, 3: 4, 4: 3}
+
 visit((0, 0))
 draw()
 
 n_minutes = 0
 while True:
-    expansion = set()
-    for pos in oxygen:
+    expanding = False
+    for pos in [p for p, v in g.items() if v == "O"]:
         for direction in [1, 2, 3, 4]:
             if direction == 1:
                 candidate_pos = (pos[0], pos[1] - 1)
@@ -168,10 +154,10 @@ while True:
                 candidate_pos = (pos[0] - 1, pos[1])
             elif direction == 4:
                 candidate_pos = (pos[0] + 1, pos[1])
-            if candidate_pos not in walls and candidate_pos not in oxygen:
-                expansion.add(candidate_pos)
-    oxygen |= expansion
-    if not expansion:
+            if g[candidate_pos] == ".":
+                g[candidate_pos] = "O"
+                expanding = True
+    if not expanding:
         break
     n_minutes += 1
 
